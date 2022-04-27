711eabaf234114a35870c6a196a8503b9bad4022
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-04-27 18:04:50 +0530

    Added die components with styling

---
+++ my-app/src/Die.jsx
@@ -0,0 +1,8 @@
+function Die(props) {
+  return (
+    <div className="die">
+      <span>{props.children}</span>
+    </div>
+  );
+}
+export default Die;
--- my-app/src/components/Main.jsx
+++ my-app/src/components/Main.jsx
@@ -1,6 +1,19 @@
+import Die from "../Die";
 function Main(props) {
   return (
     <main>
+      <div className="die--container">
+        <Die>1</Die>
+        <Die>1</Die>
+        <Die>1</Die>
+        <Die>1</Die>
+        <Die>1</Die>
+        <Die>1</Die>
+        <Die>1</Die>
+        <Die>1</Die>
+        <Die>1</Die>
+        <Die>1</Die>
+      </div>
     </main>
   );
 }

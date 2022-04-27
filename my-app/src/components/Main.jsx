4297af03a2002b16180e8264e5f93aa814789f3b
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-04-27 19:30:14 +0530

    Added isHeld boolean to the Die component

--- my-app/src/components/Main.jsx
+++ my-app/src/components/Main.jsx
@@ -1,16 +1,51 @@
 import Die from "./Die";
-
+import React from "react";
 const random = () => Math.floor(Math.random() * 6) + 1;
 
-function Main(props) {
+function genRandomdie() {
   const dieArray = [];
   for (let i = 0; i < 10; i++) {
-    dieArray.push(<Die>{random()}</Die>);
+    dieArray.push(random());
   }
+  return dieArray;
+}
+
+function Main(props) {
+  const [state, setState] = React.useState({
+    value: genRandomdie(),
+    isHeld: false,
+  });
+  function handleClick(e) {
+    setState((oldState) => {
+      return {
+        ...oldState,
+        value: genRandomdie(),
+      };
+    });
+  }
+
+  function handleIsHeld(e) {
+    setState((oldState) => {
+      return {
+        ...oldState,
+        isHeld: !oldState.isHeld,
+      };
+    });
+  }
+
   return (
-    <main>
-      <div className="die--container">{dieArray.map((ele) => ele)}</div>
-    </main>
+    <>
+      <main>
+        <div className="die--container" onClick={handleIsHeld}>
+          {state.value.map((ele) => (
+            <Die>{ele}</Die>
+          ))}
+        </div>
+        <button className="rollup--button" type="button" onClick={handleClick}>
+          Roll up
+        </button>
+      </main>
+    </>
   );
 }
 export default Main;
